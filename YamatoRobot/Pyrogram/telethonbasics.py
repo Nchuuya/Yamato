7fba16db3659b9316c70ba2068c30a9b56435aeb
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-16 10:17:51 +0530

    Update telethonbasics.py

--- YamatoRobot/Pyrogram/telethonbasics.py
+++ YamatoRobot/Pyrogram/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from MarinRobot.pyrogramee.pluginshelper import runcmd
+from YamatoRobot.pyrogramee.pluginshelper import runcmd
 
 
 async def convert_to_image(event, borg):
