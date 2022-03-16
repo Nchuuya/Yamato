646e4308c752261fc34f7211dc5a32a880933077
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-16 10:51:05 +0530

    Update antiflood.py

--- YamatoRobot/modules/antiflood.py
+++ YamatoRobot/modules/antiflood.py
@@ -1,29 +1,3 @@
-"""
-MIT License
-
-Copyright (C) 2021 Unknown-san
-
-This file is part of @MarinRobot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import html
 from typing import Optional
 
@@ -32,14 +6,14 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler
 from telegram.utils.helpers import mention_html
 
-from MarinRobot import dispatcher
-from MarinRobot.modules.connection import connected
-from MarinRobot.modules.helper_funcs.alternate import send_message, typing_action
-from MarinRobot.modules.helper_funcs.chat_status import is_user_admin, user_admin
-from MarinRobot.modules.helper_funcs.string_handling import extract_time
-from MarinRobot.modules.log_channel import loggable
-from MarinRobot.modules.redis.approvals_redis import is_approved
-from MarinRobot.modules.sql import antiflood_sql as sql
+from YamatoRobot import dispatcher
+from YamatoRobot.modules.connection import connected
+from YamatoRobot.modules.helper_funcs.alternate import send_message, typing_action
+from YamatoRobot.modules.helper_funcs.chat_status import is_user_admin, user_admin
+from YamatoRobot.modules.helper_funcs.string_handling import extract_time
+from YamatoRobot.modules.log_channel import loggable
+from YamatoRobot.modules.redis.approvals_redis import is_approved
+from YamatoRobot.modules.sql import antiflood_sql as sql
 
 FLOOD_GROUP = 3
 
