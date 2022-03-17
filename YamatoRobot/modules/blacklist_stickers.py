c1ac942417ce091e97c3cdaa321eb60d9f6338f1
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-17 07:45:36 +0530

    Update blacklist_stickers.py

--- YamatoRobot/modules/blacklist_stickers.py
+++ YamatoRobot/modules/blacklist_stickers.py
@@ -1,29 +1,3 @@
-"""
-MIT License
-
-Copyright (C) 2021 Unknown-san
-
-This file is part of @MarinRobot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import html
 from typing import Optional
 
@@ -32,17 +6,17 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
 from telegram.utils.helpers import mention_html, mention_markdown
 
-import MarinRobot.modules.sql.blsticker_sql as sql
-from MarinRobot import LOGGER, dispatcher
-from MarinRobot.modules.connection import connected
-from MarinRobot.modules.disable import DisableAbleCommandHandler
-from MarinRobot.modules.helper_funcs.alternate import send_message
-from MarinRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from MarinRobot.modules.helper_funcs.misc import split_message
-from MarinRobot.modules.helper_funcs.string_handling import extract_time
-from MarinRobot.modules.log_channel import loggable
-from MarinRobot.modules.redis.approvals_redis import is_approved
-from MarinRobot.modules.warns import warn
+import YamatoRobot.modules.sql.blsticker_sql as sql
+from YamatoRobot import LOGGER, dispatcher
+from YamatoRobot.modules.connection import connected
+from YamatoRobot.modules.disable import DisableAbleCommandHandler
+from YamatoRobot.modules.helper_funcs.alternate import send_message
+from YamatoRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from YamatoRobot.modules.helper_funcs.misc import split_message
+from YamatoRobot.modules.helper_funcs.string_handling import extract_time
+from YamatoRobot.modules.log_channel import loggable
+from YamatoRobot.modules.redis.approvals_redis import is_approved
+from YamatoRobot.modules.warns import warn
 
 
 def blackliststicker(update: Update, context: CallbackContext):
