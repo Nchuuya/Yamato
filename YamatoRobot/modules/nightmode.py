ec086fd81de160aaad07bdc76f9b17ed85214fba
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-17 10:44:26 +0530

    Update nightmode.py

--- YamatoRobot/modules/nightmode.py
+++ YamatoRobot/modules/nightmode.py
@@ -1,38 +1,11 @@
-"""
-MIT License
-
-Copyright (C) 2021 Unknown-san
-
-This file is part of @MarinRobot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telethon import *
 from telethon import functions
 from telethon.tl.types import ChatBannedRights
 
-from MarinRobot import LOGGER, OWNER_ID, telethn
-from MarinRobot.events import register
-from MarinRobot.modules.sql.night_mode_sql import (
+from YamatoRobot import LOGGER, OWNER_ID, telethn
+from YamatoRobot.events import register
+from YamatoRobot.modules.sql.night_mode_sql import (
     add_nightmode,
     get_all_chat_id,
     is_nightmode_indb,
