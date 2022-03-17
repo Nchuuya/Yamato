a23f60d8920cddfffa4aff3e7cabb93f766bdb65
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-17 18:51:10 +0530

    Create users.py

--- YamatoRobot/modules/users.py
+++ YamatoRobot/modules/users.py
@@ -1,29 +1,3 @@
-"""
-MIT License
-
-Copyright (C) 2021 Unknown-san
-
-This file is part of @MarinRobot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from io import BytesIO
 from time import sleep
 
@@ -31,10 +5,10 @@ from telegram import TelegramError, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
 
-import MarinRobot.modules.sql.users_sql as sql
-from MarinRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from MarinRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from MarinRobot.modules.sql.users_sql import get_all_users
+import YamatoRobot.modules.sql.users_sql as sql
+from YamatoRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from YamatoRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from YamatoRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
