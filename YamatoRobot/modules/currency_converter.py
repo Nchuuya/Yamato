611b9f28abed212a6b81fb19d7405ec4d3533ef9
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-17 07:44:13 +0530

    Update currency_converter.py

--- YamatoRobot/modules/currency_converter.py
+++ YamatoRobot/modules/currency_converter.py
@@ -2,7 +2,7 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from MarinRobot import CASH_API_KEY, dispatcher
+from YamatoRobot import CASH_API_KEY, dispatcher
 
 
 def convert(update: Update, context: CallbackContext):
