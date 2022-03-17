5e339291d818c686ae51119cd06298fff7ea102d
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-17 19:08:56 +0530

    Update rmbg.py

--- YamatoRobot/modules/rmbg.py
+++ YamatoRobot/modules/rmbg.py
@@ -1,29 +1,3 @@
-"""
-MIT License
-
-Copyright (C) 2021 Unknown-san
-
-This file is part of @MarinRobot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import io
 import os
 from datetime import datetime
@@ -32,8 +6,8 @@ import requests
 from telethon import types
 from telethon.tl import functions
 
-from MarinRobot import REM_BG_API_KEY, TEMP_DOWNLOAD_DIRECTORY, telethn
-from MarinRobot.events import register
+from YamatoRobot import REM_BG_API_KEY, TEMP_DOWNLOAD_DIRECTORY, telethn
+from YamatoRobot.events import register
 
 
 async def is_register_admin(chat, user):
