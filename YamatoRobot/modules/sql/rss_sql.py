1bd839ef154c24d211223d649a129f53765d2fff
Yoriichi <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-03-17 16:45:16 +0530

    Update rss_sql.py

--- YamatoRobot/modules/sql/rss_sql.py
+++ YamatoRobot/modules/sql/rss_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, UnicodeText
 
-from MarinRobot.modules.sql import BASE, SESSION
+from YamatoRobot.modules.sql import BASE, SESSION
 
 
 class RSS(BASE):
