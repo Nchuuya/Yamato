8951a256add290e2c45a36e18020d522b852a889
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-18 08:40:00 +0530

    Update errors.py

--- YamatoRobot/utils/errors.py
+++ YamatoRobot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from MarinRobot import SUPPORT_CHAT, pbot
+from YamatoRobot import SUPPORT_CHAT, pbot
 
 
 def split_limits(text):
