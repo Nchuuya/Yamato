c0038914702aa6d0e98d6157fd4a7a874800ab67
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-18 08:17:57 +0530

    Update disable.py

--- YamatoRobot/modules/disable.py
+++ YamatoRobot/modules/disable.py
@@ -21,12 +21,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from MarinRobot.modules.helper_funcs.chat_status import (
+    from YamatoRobot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from MarinRobot.modules.sql import disable_sql as sql
+    from YamatoRobot.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
