51d8a3a6ada222d1e1e16113738dd0c759e61567
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-18 08:46:45 +0530

    Update disable.py

--- YamatoRobot/modules/disable.py
+++ YamatoRobot/modules/disable.py
@@ -150,7 +150,7 @@ if is_module_loaded(FILENAME):
         args = context.args
         chat = update.effective_chat
         if len(args) >= 1:
-            disable_module = "MarinRobot.modules." + args[0].rsplit(".", 1)[0]
+            disable_module = "YamatoRobot.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(disable_module)
@@ -224,7 +224,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
 
         if len(args) >= 1:
-            enable_module = "MarinRobot.modules." + args[0].rsplit(".", 1)[0]
+            enable_module = "YamatoRobot.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(enable_module)
