e1e3b0a6788a19f1b498e00ca574fcfd7aa8720f
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-18 11:28:15 +0530

    Update __main__.py

--- YamatoRobot/__main__.py
+++ YamatoRobot/__main__.py
@@ -145,7 +145,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("MarinRobot.modules." + module_name)
+    imported_module = importlib.import_module("YamatoRobot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
