7ac988f9eead4354416da2b7493e492f68c71366
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-18 11:01:41 +0530

    Update dbfunctions.py

--- YamatoRobot/utils/dbfunctions.py
+++ YamatoRobot/utils/dbfunctions.py
@@ -2,7 +2,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from MarinRobot.mongo import db
+from YamatoRobot.mongo import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
