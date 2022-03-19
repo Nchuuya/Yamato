21c0cc7cd67cd05fb73d15ea1565fd0070e9651f
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-19 09:21:37 +0530

    Update __main__.py

--- YamatoRobot/__main__.py
+++ YamatoRobot/__main__.py
@@ -81,7 +81,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 *Hello {} !*
-✪ I'm Yamato Hentai-theme management bot [✨](https://telegra.ph/file/88263492876f6c2d5b995.jpg)
+✪ I'm Yamato Hentai-theme management bot [✨](https://telegra.ph/file/00409644b0a6b670bc320.jpg)
 ────────────────────────
 × *Uptime:* `{}`
 × `{}` *users, across* `{}` *chats.*
@@ -101,7 +101,7 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="➗ Add Yamato To Your Group ➗", url=f"t.me/{bu}?startgroup=new"),
+            text="➗ Add Yamato To Your Group ➗", url=f"t.me/{}?startgroup=new"),
     ],
 ]
 
