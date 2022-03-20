d8884e1363c068a8b590795fc1a0ac22a2d7fdcf
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-20 20:58:12 +0530

    Update truth_and_dare.py

--- YamatoRobot/modules/truth_and_dare.py
+++ YamatoRobot/modules/truth_and_dare.py
@@ -20,13 +20,22 @@ def sigma(update: Update, context: CallbackContext):
     update.effective_message.reply_video(random.choice(truth_and_dare_string.SIGMA))
 
 
+def cosplay(update: Update, context: CallbackContext):
+    update.effective_message.reply_image(random.choice(truth_and_dare_string.COSPLAY))
+
+
+
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
 SIGMA_HANDLER = DisableAbleCommandHandler("sigma", sigma, run_async=True)
+COSPLAY_HANDLER = DisableAbleCommandHandler("sigma", sigma, run_async=True)
+
 
 
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 dispatcher.add_handler(SIGMA_HANDLER)
+dispatcher.add_handler(COSPLAY_HANDLER)
+
