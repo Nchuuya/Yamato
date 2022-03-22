fc554c6ec6708d8c195417217eaca36692ace280
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-22 22:11:00 +0530

    Update __main__.py

--- YamatoRobot/__main__.py
+++ YamatoRobot/__main__.py
@@ -424,7 +424,8 @@ def shasa_callback_data(update, context):
         )
     elif query.data == "shasa_basichelp":
         PHOTO="https://telegra.ph/file/3802a957ef2e3e584248c.jpg"
-        query.message.edit_text(
+        query.message.edit_caption(
+            PHOTO,
             text="*ʜᴇʏᴏ! ᴡᴀᴛᴀsʜɪᴡᴀ ʏᴀᴍᴀᴛᴏ*"
             "\n ɪ ᴀᴍ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ.\nᴛʜɪꜱ ɪꜱ ʜᴇɴᴛᴀɪ ꜱᴛᴀʀᴛ!\n"
             "\n ᴜ ᴄᴀɴ ɢᴏ ʙᴀᴄᴋ ᴏʀ ᴄʜᴏᴏꜱᴇ ᴏɴᴇ ᴏꜰ ᴛʜᴇ ᴏᴘᴛɪᴏɴꜱ ʙᴇʟᴏᴡ!\n"
