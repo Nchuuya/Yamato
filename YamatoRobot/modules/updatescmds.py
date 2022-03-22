f8aceda9d5dffe75f80cd3063c1be65df70cda80
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-22 17:33:07 +0530

    zerohisoka special'

---
+++ YamatoRobot/modules/updatescmds.py
@@ -0,0 +1,7 @@
+__mod_name__ = "UPDATE NEW CMDS"
+
+__help__ = """
+ |• `/cosplay`: Genrates Random Cosplay
+ |• `/waifu ` : Genrates Random Waifus of Popular/Big Animes 
+ |• `/sigma`  : Genrates Random Anime Baesd Sigma Rules
+"""
